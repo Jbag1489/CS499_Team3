406a685703de66ae5a3d9e424734489bc0d4d130
Josh Bagwell <Jbag1489@gmail.com>
2017-02-06 17:15:35 -0600

    Initial GUI Components
    This should also fix the problem of assets not properly showing up. If
    it doesn't will need to figure that out. I had to make a new project and
    copy all code into it.

--- CS499PirateSimJME/src/PirateSim/Application.java
+++ CS499PirateSimJME/src/PirateSim/Application.java
@@ -2,10 +2,13 @@ package PirateSim;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.math.Vector3f;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.system.AppSettings;
 import com.jme3.renderer.RenderManager;
+import de.lessvoid.nifty.Nifty;
 
 public class Application extends SimpleApplication {
+
     Simulation sim;
     Scene scene;
     PanCamera panCam;
@@ -13,6 +16,7 @@ public class Application extends SimpleApplication {
     float timeAcceleration = 1;
     static final int targetFPS = 30;
     boolean simPaused = false;
+    private MyStartScreen startScreen;
 
     public static void main(String[] args) {
         Application app = new Application();
@@ -27,6 +31,7 @@ public class Application extends SimpleApplication {
         setSettings(settings);
         start();
     }
+
     @Override
     public void simpleInitApp() {
         sim = new Simulation(20, 10, 0.4, 0.25, 0.2, 6545);
@@ -34,20 +39,41 @@ public class Application extends SimpleApplication {
         panCam = new PanCamera(cam, inputManager, getFlyByCamera());
         panCam.register();
         timeSinceLastFrame = 0;
+
+
+        /* Josh Test Addition */
+        startScreen = new MyStartScreen();
+        stateManager.attach(startScreen);
+
+        /**
+         * Åctivate the Nifty-JME integration:
+         */
+        NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(
+                assetManager, inputManager, audioRenderer, guiViewPort);
+        Nifty nifty = niftyDisplay.getNifty();
+        guiViewPort.addProcessor(niftyDisplay);
+        nifty.fromXml("Interface/newNiftyGui.xml", "start", startScreen);
+        //nifty.setDebugOptionPanelColors(true);
+
+        flyCam.setDragToRotate(true); // you need the mouse for clicking now 
+        
+        /* Josh Test Addition End */
     }
+
     @Override
     public void simpleUpdate(float tpf) {
         if (!simPaused) {
             timeSinceLastTick += tpf;
             timeSinceLastFrame += tpf;
         }
-        while (timeSinceLastTick > 1/timeAcceleration) {
+        while (timeSinceLastTick > 1 / timeAcceleration) {
             sim.tick();
-            timeSinceLastTick -= 1/timeAcceleration;
+            timeSinceLastTick -= 1 / timeAcceleration;
         }
-        float alpha = timeSinceLastTick*timeAcceleration;
+        float alpha = timeSinceLastTick * timeAcceleration;
         scene.update(alpha);
     }
+
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
